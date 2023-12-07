d8530b5254df6bf2e5f37f53b66a4b2fa3c58de2
Adventureandre <adventureandre@hotmail.com>
2023-12-06 22:38:11 -0300

    Separando o projeto por Screens!

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Text, View} from "react-native";
+import {styles} from "./styles";
+
+export default function Home() {
+    return (
+        <View style={styles.conteiner}>
+            <Text style={styles.eventName}>
+                Nome do evento
+            </Text>
+
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022
+            </Text>
+        </View>
+
+
+    )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native'
+export const styles = StyleSheet.create({
+    conteiner: {
+        flex: 1,
+        backgroundColor: '#160c18',
+        padding: 24,
+    },
+    eventName: {
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+    },
+    eventDate:{
+        color:'#6B6B6B',
+        fontSize:16
+    }
+});
